b1ddffdddd30673ffa8a27f3f603800b0ed2d763
Nikita <gaikovgnv@gmail.com>
2020-11-13 13:04:46 +0300

    master start

---
+++ src/main/java/com/NikitaGaikov/ProjectSimbirsoft/ProjectSimbirsoftApplication.java
@@ -0,0 +1,13 @@
+package com.NikitaGaikov.ProjectSimbirsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSimbirsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSimbirsoftApplication.class, args);
+	}
+
+}
