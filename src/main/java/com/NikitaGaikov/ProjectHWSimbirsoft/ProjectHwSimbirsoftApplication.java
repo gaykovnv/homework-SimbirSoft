077bd48647550e71d33f1b1b0e02ccb61faefac0
Nikita <gaikovgnv@gmail.com>
2020-11-13 11:41:28 +0300

    start work

---
+++ src/main/java/com/NikitaGaikov/ProjectHWSimbirsoft/ProjectHwSimbirsoftApplication.java
@@ -0,0 +1,13 @@
+package com.NikitaGaikov.ProjectHWSimbirsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHwSimbirsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHwSimbirsoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NikitaGaikov/ProjectHWSimbirsoft/ProjectHwSimbirsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NikitaGaikov.ProjectHWSimbirsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHwSimbirsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
